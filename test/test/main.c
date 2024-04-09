7dbff84f5bd1e6cf0a1a94147a357ca1e0d59d9d
Bauer Kirill <bk.evg@mail.ru>
2024-04-10 02:26:01 +0700

    init

---
+++ test/test/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  test
+//
+//  Created by Кирилл Бауэр on 10.04.2024.
+//
+
+#include <stdio.h>
+#include "stdlib.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("HELLO!!!!\n");
+    return 0;
+}
