29de0014ef046f4c30ca79630ee52cdf269831c7
Bauer Kirill <bk.evg@mail.ru>
2024-04-17 21:42:11 +0700

    restore files

---
+++ first_project/first_project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  first_project
+//
+//  Created by Кирилл Бауэр on 17.04.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
