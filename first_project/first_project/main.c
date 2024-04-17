21521ffde9e7af4485efcecccd94e01f094becce
Bauer Kirill <bk.evg@mail.ru>
2024-04-17 22:27:30 +0700

    implemented string concat

--- first_project/first_project/main.c
+++ first_project/first_project/main.c
@@ -6,9 +6,33 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void strip_string(char* str, int size){
+    int i;
+    for (i = 0; i<=size; ++i) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+        }
+    }
+}
+
+int main(void){
+    char first_name[50];
+    char last_name[50];
+    char full_name[100];
+    
+    printf("Программа включена\nВведите свое имя: ");
+    fgets(first_name, 50, stdin);
+    strip_string(first_name, 50);
+    printf("Введите свою фамилию: ");
+    fgets(last_name, 50, stdin);
+    strip_string(last_name, 50);
+    full_name[0] = '\0';
+    strncat(full_name, first_name, 100);
+    strncat(full_name, " ", 100);
+    strncat(full_name, last_name, 100);
+    printf("%s", full_name);
     return 0;
 }
