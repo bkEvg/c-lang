1f95c49d07d3b80a1d116c5ae7bbb8330a5e6a2f
Bauer Kirill <bk.evg@mail.ru>
2024-04-18 21:34:14 +0700

    better implementation of find func

--- first_project/first_project/main.c
+++ first_project/first_project/main.c
@@ -10,9 +10,26 @@
 #include <string.h>
 
 
+int* find(int *numbers, int size, int target){
+    int *result;
+    for (int i = 0; i < size; i++) {
+        if (*(numbers+i) == target) {
+            result = numbers+i;
+            return result;
+        }
+    }
+    return NULL;
+}
+
 int main(void){
-    const char *strConst = "Hell boy";
-    printf("%s", strConst);
+    int numbers[] = {57,6,54,8,67,3};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int *search_num = find(numbers, size, 8);
+    if (search_num != NULL) {
+        printf("Позиция символа который надо найти (%d): %p\n", 8, search_num);
+        printf("Индекс в строке: %d\n", search_num - numbers);
+    } else {
+        printf("Число не найдено!\n");
+    }
     return 0;
-    
 }
